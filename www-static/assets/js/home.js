8318e70255ba7cd105db19059eaa7c4bd3c4afda
Adam Ramadhan <adamramadhan@live.com>
2011-02-16 22:41:34 +0700

    updated why page and some things

--- www-static/assets/js/home.js
+++ www-static/assets/js/home.js
@@ -60,9 +60,9 @@ $('#pic-1 img').load(function() {
 	$('#pic-2').css('opacity',0);
 	$('#pic-3').css('opacity',0);
 		$('body').mousemove(function() {	
-		$('#pic-1').delay(0).animate({opacity: 1}, 2600 );	
-		$('#pic-2').delay(500).animate({opacity: 1}, 2600 );
-		$('#pic-3').delay(1000).animate({opacity: 1}, 2600 );
+		$('#pic-1').delay(0).animate({opacity: 1}, 500 );	
+		$('#pic-2').delay(600).animate({opacity: 1}, 500 );
+		$('#pic-3').delay(1200).animate({opacity: 1}, 500 );
 		});
 	});
 
