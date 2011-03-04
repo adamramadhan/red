92eb09e666d7fef8c683352e9006dcd180fe6c22
Adam Ramadhan <adamramadhan@live.com>
2011-03-04 23:05:10 +0700

    add hints for transtation

--- application/views/site/why.php
+++ application/views/site/why.php
@@ -1,36 +1,33 @@
+	<style type="text/css" media="screen">
+		#why p{text-shadow:0 1px #F7F7F7;font:133%/1.6em Verdana,sans-serif;}
+		#why
+		{
+			background: none repeat scroll 0 0 #FFF8DE;
+			margin-bottom:5px;padding: 5px;
+    	}
+    	#how{    border-top: 1px solid #EEEEEE;
+    margin-bottom: 5px;
+    padding: 5px;}
+	</style>
+
 	<!-- CONTENT START -->
 	<div id="red-content">
-		<h3>Instant</h3>
-		<ul id="why">
-			<li><p>Anda dapat langsung membuat profile organisasi anda, organisasi bisnis ( pt, cv, dan lainnya )</p></li>
-			<li><p>Sekarang, anda dapat fokus kepada hal yang lebih penting, tampa harus terbentur infrastruktur teknologi</p></li>
-			<li><p>Cepat, data anda berada dalam jaringan ( iix ) dalam negri, tepatnya pada IDC ( indonesian Datacenter )</p></li>
-		</ul><hr/>
-		<img src="http://dl.dropbox.com/u/5984602/netcoid/1.png" />
-		<hr/>
-		<img src="http://dl.dropbox.com/u/5984602/netcoid/5.png" />
-		<hr/>	
-		<h3>Koneksi</h3>
-		<ul id="why">
-			<li><p>Gabungkan profile anda dengan, akun twitter dan yahoo messenger anda</p></li>
-			<li><p>Hubungkan, organisasi anda dengan komunitas lainnya. dan tingkatkan penjualan anda</p></li>
-			<li><p>Kembangkan, jaringan anda dalam secara local dan mancan negara melewati www ( world wide web )</p></li>
-		</ul><hr/>
-		<img src="http://dl.dropbox.com/u/5984602/netcoid/2.png" />
-		<h3>Mudah</h3>
-		<ul id="why">
-			<li><p>Tips & Guide pada setiap halaman, yang membantu anda bernavigasi</p></li>
-			<li><p>Tanyakan, Kami juga memberikan helpdesk yang tersedia jika anda mendapatkan kesulitan</p></li>
-		</ul><hr/>
-		<img src="http://dl.dropbox.com/u/5984602/netcoid/3.png" />	
-		<h3>Detail</h3>
-		<ul id="why">
-			<li><p>Verifikasi, setiap usaha memiliki sistim badge: unverified, verified, badge power, dan sebagainya</p></li>
-			<li>
-				<p>Sistim, Kami yang tidak ingin menghambat anda dalam bertransaksi, langsung hubungi, dan langsung dapat bertransaksi tampa kami</p>
-				<p><strong>Cari, Pilih, Kontak, <em>Bertemu</em>, Transaksi, Follow</strong></p>
-			</li>
-		</ul><hr/>
-		<img src="http://dl.dropbox.com/u/5984602/netcoid/4.png" />		
+		<div class="whybox" id="why"><p>Netcoid dibentuk sebagai media, ispirasi, dan menguatkan entureprainer yang berada disetiap daerah indonesia. Didirikan pada 28 April 2010, Netcoid adalah jejaring bisnis pertama diindonesia, dengan menggabungkan semua ide yang terbaik yang pernah ada di web, mulai dari social, profiles, dan sistim pencarian.</p></div>
+
+		<div class="whybox" id="how">Sebagai destinasi komunitas, Netcoid adalah sebuah layanan dalam bentuk aplikasi yang membantu saling <strong>menghubungkan, berinteraksi, dan bertransaksi</strong> yang dapat digunakan hingga ke organisasi <a href="http://en.wikipedia.org/wiki/Nonprofit_organization">non-profit</a>, dengan cara <strong>semudah, sedekat dan sesimple</strong> mungkin.
+		Saat ini kami juga membuka kerjasama kepada komunitas non-profit ( seperti komunitas pengrajin, komunitas kalangan wiraswasta mahasiswa, atau komunitas non-profit maupun profit lainnya ), jika berminat, silahkan email ke nonprofit@networks.co.id.
+		</div>
+
+		<div>
+		<h2>Seperti Apa?</h2>
+		<h3>Profile</h3>
+		<?php $views->getIMG('what-1.png'); ?>
+		<h3>Beranda</h3>
+		<?php $views->getIMG('what-2.png'); ?>
+		<h3>Products</h3>
+		<?php $views->getIMG('what-3.png'); ?>
+		<h3>Lainnya?</h3>
+		anda dapat melihat informasi lainnya di blog netcoid http://networks.co.id/blog atau dengan langsung mendaftarkan diri sekarang. untuk informasi ke hello@networks.co.id
+		</div>
 	</div>
 	<!-- CONTENT END -->
