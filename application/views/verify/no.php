ae7e84129e8b810b3661caf6e637971b8645a3e1
Adam Ramadhan <adamramadhan@live.com>
2011-03-08 19:46:42 +0700

    major update /verify

--- application/views/verify/no.php
+++ application/views/verify/no.php
@@ -1,15 +1,6 @@
 	<!-- CONTENT START -->
 	<div class="clearfix background" id="red-content">
 		<div id="verified">
-			<ul>
-				<li class="clearfix"><label>Nama</label><p><?php echo $user['name']; ?></p></li>
-				<li class="clearfix"><label>Status</label><p>TIDAK TERVERIFIKASI</p></li>
-				<?php if (!empty($user['address'])): ?>
-					<li class="clearfix"><label>Alamat</label><p><?php echo $user['address']; ?></p></li>
-					<li class="clearfix"><?php $this->googlemaps->show(); ?></li>
-				<?php endif ?>	
-				<li class="clearfix"><label>Kontak</label><p>08999252530</p></li>
-			</ul>
 			<ul class="c" id="notverified">
 				<li class="clearfix"><label>Identity NOT Verified</label><p><strong><?php echo $user['name']; ?></strong> <span id="important">is not verified</span> as the owner or operator of the Web site located at www.networks.co.id/<?php echo $user['username']; ?>. <span id="important">No official records</span> confirm <strong><?php echo $user['name']; ?></strong> as a valid business or an non-profit organization. If you are the owner or legal officials please contact hello@networks.co.id.</p>
 				</li>
