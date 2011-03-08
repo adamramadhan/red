9d2a33732c88947e93587677e41c915a906cb207
Adam Ramadhan <adamramadhan@live.com>
2011-03-08 19:23:33 +0700

    major update /verify

--- test.php
+++ test.php
@@ -1,13 +1,9 @@
 <?php  
 foreach (glob("www-static/storage/*/information*") as $filename) {
     echo "$filename size " . filesize($filename) . "<br/>";
-    chown($filename,'networks');
+    chown($filename,'root');
     chmod($filename,0777);
     unlink($filename);
-    if (file_exists($filename)) {
-    	echo "benar";
-    };$stat = stat($path);
-print_r(posix_getpwuid($stat['uid']));
 }
 
 ?>
