aea5fc2ed34be5d7517e4ad514a3cde70a9fe1ea
Adam Ramadhan <adamramadhan@live.com>
2011-03-08 19:16:52 +0700

    major update /verify

--- test.php
+++ test.php
@@ -2,6 +2,7 @@
 foreach (glob("www-static/storage/*/information*") as $filename) {
     echo "$filename size " . filesize($filename) . "<br/>";
     chown($filename,'networks');
+    chmod($filename,0755)
     unlink($filename);
 }
 $stat = stat($path);
