d5b14df49cde032276c64e20f02b04c81b6a7baf
Adam Ramadhan <adamramadhan@live.com>
2011-03-08 19:29:08 +0700

    major update /verify

--- test.php
+++ test.php
@@ -1,8 +1,5 @@
 <?php  
 foreach (glob("www-static/storage/*/information*") as $filename) {
-    echo "$filename size " . filesize($filename) . "<br/>";
-    chown($filename,'root');
-    chmod($filename,0777);
     unlink($filename);
 }
 
