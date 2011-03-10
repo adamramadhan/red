9fd8712e2e3d420b96e189d0d8d57dcca16db163
Adam Ramadhan <adamramadhan@live.com>
2011-03-10 10:55:28 +0700

    fix chmod images #1

--- application/views/verify/index.php
+++ application/views/verify/index.php
@@ -2,7 +2,7 @@
 	<div class="clearfix background-verify" id="red-content">
 		<div id="box-verify">
 			<h3>Kenapa Verifikasi?</h3>
-			<p>Semua akun yang mempunyai <i>Verified Badge</i> adalah akun yang terverifikasi. netcoid menggunakan sistim ini untuk memastikan keaslian dari akun tertentu sehingga user atau pengunjung dapat memberikan rasa <strong>kepercayaan</strong> bahwa akun ini dikelola oleh sumber yang diakui. contoh: <a target="_blank" href="/verify/LVUstore">LVU STORE</a> atau <a href="/mangkukmerah">Mangkuk Merah Hosting</a></p>
+			<p>Semua akun yang mempunyai <i>Verified Badge</i> adalah akun yang terverifikasi. netcoid menggunakan sistim ini untuk memastikan keaslian dari akun tertentu sehingga user atau pengunjung dapat memberikan rasa <strong>kepercayaan</strong> bahwa akun ini dikelola oleh sumber yang diakui. contoh: <a target="_blank" href="/verify/LVUstore">LVU STORE</a> atau <a href="verify/mangkukmerah">Mangkuk Merah Hosting</a></p>
 		</div>
 		<div id="box-verify">
 			<h3>Bagaimana?</h3>
