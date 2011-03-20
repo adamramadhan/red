19c33db2f90319ed3f525ae2472348583710130a
Adam Ramadhan <adamramadhan@live.com>
2011-03-21 03:14:47 +0700

    major update #2 getting ready for batch #1 rabu ke 3d

--- application/views/site/login.php
+++ application/views/site/login.php
@@ -15,7 +15,7 @@
 		<div id="red-login-information">
 			<h3>Partnerships</h3>
 			<?php $views->getIMG('icon-domain-id.gif') ?>
-			<a rel="nofollow" href="http://xcode.or.id/"><?php $views->getIMG('partnerships/yogyafree-banner.png') ?></a>
+			<a rel="nofollow" href="http://xcode.or.id/support.htm"><?php $views->getIMG('partnerships/yogyafree-banner.png') ?></a>
 		</div>
 	</div>
 	<!-- CONTENT END -->
