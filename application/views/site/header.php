fd44c550280e965a713952925ff3c9e91bd15bc0
Adam Ramadhan <adamramadhan@live.com>
2011-03-21 03:10:04 +0700

    major update #2 getting ready for batch #1 rabu ke 3d

--- application/views/site/header.php
+++ application/views/site/header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php $views->css('framework','netcoid.v1','home'); ?>
 		<meta charset="utf-8">
 		<meta name="copyright" content="netcoid 2010" />
 		<meta name="description" content="netcoid, jejaring bisnis indonesia, temukan partner baru di sekitar anda." />
 		<meta name="keywords" content="jejaring bisnis, social business, partner usaha, usaha indonesia" />
 		<title>netcoid &mdash; jejaring bisnis indonesia</title>
+		<?php $views->css('framework','netcoid.v1','home'); ?>
 	</head>
 <body class="<?php $views->ActiveCSS(); ?>">
