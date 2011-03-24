4731d40481bacc9eeecd32440d4caec8afb5c617
Adam Ramadhan <adamramadhan@live.com>
2011-03-24 22:31:46 +0700

    ssl

--- application/views/site/register-faktanya.php
+++ application/views/site/register-faktanya.php
@@ -7,12 +7,19 @@
 				Hello Faktahunter, Lets start to make a <strong>change</strong>.
 			</div>
 			<ul>
-				<li style="padding: 10px;">netcoid dibentuk sebagai media, ispirasi dan menguatkan jiwa entrepreneur indonesia, disini faktahunter dapat membuat profil toko, usaha atau organisasi sendiri untuk menawarkan produk <strong>karya original</strong> masing masing, faktahunter bisa saling berinteraksi dengan toko lainnya. Lengkapnya langsung ke <a style="color:#D32E2E" target="_blank" href="/why">Perkenalan</a>.</li>
+				<li style="padding: 10px;">@netcoid dibentuk sebagai media, ispirasi dan menguatkan jiwa entrepreneur indonesia, disini faktahunter dapat membuat profil toko, usaha atau organisasi sendiri untuk menawarkan produk <strong>karya original</strong> masing masing, faktahunter bisa saling berinteraksi dengan toko lainnya. Lengkapnya langsung ke <a style="color:#D32E2E" target="_blank" href="/why">Perkenalan</a>.</li>
 				<li id="red-register-closed">1. masukan username yang diinginkan,nanti digunakan sebagai url anda seperti www.networks.co.id/username<li>
-				<li id="red-register-closed">2. kata sandi, pastinya tidak kami simpan secara langsung ( di hash / enskirpsi satu jalan ) kalo lupa
-					passwordnya cuma bisa direset. 'password' akan terlihat seperti ini di database<img src="http://dl.dropbox.com/u/5984602/images/hash.png" /> bisa sampai 200 char dan selalu diacak.<li>
-				<li id="red-register-closed">3. support di 08999252530 untuk support / gagal login karena password salah ( reset ) atau ke ym netcoid atau damsprivate. jika mengunakan bb bisa masuk ke group bb kami.<li><img src="http://dl.dropbox.com/u/5984602/images/barcode.png" />
-				<li id="red-register-closed">4. Thankyou.</li>	
+				<li id="red-register-closed">2. masukan kata sandi, <strong>kami tidak menyimpan password anda</strong> kunjungi <a href="/terms"><u>Persyaratan & Privacy</u></a> terlebih dahulu, dikarenakan hal tersebut kami hanya dapat <i>mereset password</i> anda. jika lupa silahkan hubungi <a href="/help"><u>Pusat Bantuan</u></a>.<li>
+				<li id="red-register-closed">3. Masukan nama usaha pribadi, perusahaan atau organisasi, pastikan bahwa nama anda benar.<li>
+				<li id="red-register-closed">4. Masukan nomer kontak utama, perhatikan format pengisian seperti 000-1234567 atau Kontak GSM 08123456789.<li>
+				<script src="http://platform.twitter.com/anywhere.js?id=VgMhY8zm9QF6SgpYskmptA&v=1" type="text/javascript"></script> 
+				  <script type="text/javascript">
+				    twttr.anywhere(function (T) {
+				      T.hovercards();
+				    });
+				  </script>
+				<li id="red-register-closed">5. support tambahan berada di YM: netcoid, email: help@networks.co.id ( pioritas ), atau DM / MENTION di @netcoid</a>
+				<li style=" padding: 20px 10px 0;">jangan lupa untuk follow akun official <a class="u" href="/faktanyaadalah">faktanyaadalah</a> dinetcoid untuk mendapatkan informasi merchandise sewaktu-waktu dari @faktanyaadalah!.<li>
 			</ul>
 		</div>
 		<div id="red-content-right">
