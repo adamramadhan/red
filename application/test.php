16357bc88da2a834bb8fcc4670eeadd7d5fd3af4
Adam Ramadhan <adamramadhan@live.com>
2011-03-27 12:18:01 +0700

    binusan

--- application/test.php
+++ application/test.php
@@ -28,11 +28,9 @@ class Test extends Application {
 		echo $this->sessions->get('login');
 	}
 	function info(){
-		echo ini_get('suhosin.session.encrypt');
-		echo 'display_errors = ' . ini_get('display_errors') . "\n";
-		echo 'register_globals = ' . ini_get('register_globals') . "\n";
-		echo 'post_max_size = ' . ini_get('post_max_size') . "\n";
-		echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "\n";
+		echo ini_get('suhosin.session.encrypt'). "<br/>";
+		echo 'post_max_size = ' . ini_get('post_max_size') . "<br/>";
+		echo 'post_max_size+1 = ' . (ini_get('post_max_size')+1) . "<br/>";
 		phpinfo();
 	}
 }
