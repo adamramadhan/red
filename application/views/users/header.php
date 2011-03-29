ee3eafd5f9c017c2e90b1c5faf18a50cc648cfea
Adam Ramadhan <adamramadhan@live.com>
2011-03-29 08:53:54 +0700

    hello

--- application/views/users/header.php
+++ application/views/users/header.php
@@ -11,4 +11,4 @@
 <body>
 
 <div style="height: 30px; background: none repeat scroll 0pt 0pt rgb(255, 255, 255); border-bottom: 1px solid rgb(231, 231, 231); color: rgb(68, 68, 68);"><p style="margin: 0pt auto; width: 960px; text-align: center; line-height: 29px;">
-<span style="position: relative;right: 5px;top: 2px;"><?php $views->getIMG('i/megaphonebybigfunkychiken.gif'); ?> </span>Hallo, Kami sedang voting <a href="/blog"><u>permintaan fitur #1</u></a> . Jangan lupa untuk <i>Ctrl + D</i> untuk Bookmark!</p></div>
+<span style="position: relative;right: 5px;top: 2px;"><?php $views->getIMG('i/megaphonebybigfunkychiken.gif'); ?> </span>Hallo, Kami sedang voting <a href="/blog?id=6"><u>permintaan fitur #1</u></a> . Jangan lupa untuk <i>Ctrl + D</i> untuk Bookmark!</p></div>
