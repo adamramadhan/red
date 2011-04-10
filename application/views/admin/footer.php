ecd76a4eba73fff72161aed3ace3e23f8f2681ad
Adam Ramadhan <adamramadhan@live.com>
2011-04-10 15:54:40 +0700

    fix gpp dipisah aja content blm di render sama sudah dirender dari database

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -19,7 +19,6 @@
 	</body>
 
 	<link rel="stylesheet" type="text/css" href="/www-static/assets/js/middleware/wmd/wmd.css"/>
-	<?php $views->js('middleware/wmd/wmd'); ?>
 	<?php $views->js('middleware/wmd/showdown'); ?>
 	<script type="text/javascript">
             setup_wmd({
