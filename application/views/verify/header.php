539060435da74f4fde8610801c8a634990af7936
Adam Ramadhan <adamramadhan@live.com>
2011-04-12 22:10:47 +0700

    uji coba

--- application/views/verify/header.php
+++ application/views/verify/header.php
@@ -11,3 +11,31 @@
 		<link rel="icon" type="image/png" href="/www-static/assets/images/f48x48.png" />
 	</head>
 <body class="<?php $views->activeCSS(); ?>">
+
+
+  <!-- [if IE] -->
+    <script type="text/javascript" 
+     src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
+
+    <style>
+     .chromeFrameInstallDefaultStyle {
+       width: 100%; /* default is 800px */
+       border: 5px solid blue;
+     }
+    </style>
+
+    <div id="prompt">
+     <!-- if IE without GCF, prompt goes here -->
+    </div>
+ 
+    <script>
+     // The conditional ensures that this code will only execute in IE,
+     // Therefore we can use the IE-specific attachEvent without worry
+     window.attachEvent("onload", function() {
+       CFInstall.check({
+         mode: "inline", // the default
+         node: "prompt"
+       });
+     });
+    </script>
+  <!-- [endif] -->
