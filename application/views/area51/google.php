a63712a387a432e4d83a56d7bd7e4efd40a8bdef
Adam Ramadhan <adamramadhan@live.com>
2011-04-12 22:30:27 +0700

    uji coba

--- application/views/area51/google.php
+++ application/views/area51/google.php
@@ -2,7 +2,7 @@
 
 <html>
 <body>
-  <!--[if IE]-->
+  <!--[if IE]>
     <script type="text/javascript" 
      src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
 
@@ -33,7 +33,7 @@
        });
      });
     </script>
-  <!--[endif]-->
+  <![endif]-->
 </body>
 </html>
 
