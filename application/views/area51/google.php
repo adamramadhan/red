43fdd869bd3c64cdc289bec98691e60682f01340
Adam Ramadhan <adamramadhan@live.com>
2011-04-12 22:45:07 +0700

    uji coba

--- application/views/area51/google.php
+++ application/views/area51/google.php
@@ -2,37 +2,30 @@
 
 <html>
 <body>
+    <script type="text/javascript" 
+     src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
 
-	    <script type="text/javascript" 
-	     src="https://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
+    <style>
+     .chromeFrameInstallDefaultStyle {
+       width: 100%; /* default is 800px */
+       border: 5px solid blue;
+     }
+    </style>
 
-	    <style>
-	     .chromeFrameInstallDefaultStyle {
-	       width: 100%; /* default is 800px */
-	       border: 5px solid blue;
-	     }
-	    </style>
-
-	    <div id="google-frame-noinstall">
-	     hello install google iframe untuk tampilan lebih cepat!.
-	    </div>
-
-	    <div id="google-frame-installed">
-	     terimakasih
-	    </div>
-	 
-	    <script>
-	     // The conditional ensures that this code will only execute in IE,
-	     // Therefore we can use the IE-specific attachEvent without worry
-	     window.attachEvent("onload", function() {
-	       CFInstall.check({
-	         mode: "overlay", // the default
-	         url: "https://www.google.com/chromeframe"
-	         oninstall: "google-iframe-installed",
-	         onmissing: "google-frame-noinstall"
-	       });
-	     });
-	    </script>
+    <div id="prompt">
+     <!-- if IE without GCF, prompt goes here -->
+    </div>
+ 
+    <script>
+     // The conditional ensures that this code will only execute in IE,
+     // Therefore we can use the IE-specific attachEvent without worry
+     window.attachEvent("onload", function() {
+       CFInstall.check({
+         mode: "inline", // the default
+         node: "prompt"
+       });
+     });
+    </script>
 
 </body>
 </html>
