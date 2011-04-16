205444763ad139a12e6fa03a9cb23fcdb843c41c
Adam Ramadhan <adamramadhan@live.com>
2011-04-17 00:33:40 +0700

    why update

--- application/views/site/why.php
+++ application/views/site/why.php
@@ -2,30 +2,66 @@
 		#why p{text-shadow:0 1px #F7F7F7;font:133%/1.6em Verdana,sans-serif;}
 		#why
 		{
-			background: none repeat scroll 0 0 #FFF8DE;
-			margin-bottom:5px;padding: 5px;
+		    background: none repeat scroll 0 0 #FFF8DE;
+		    border: 1px solid #CBC5AD;
+		    margin: 0 auto;
+		    padding: 5px 20px;
     	}
-    	#how{    border-top: 1px solid #EEEEEE;
+    	#how{    background: none repeat scroll 0 0 #F0F0F0;
+    border: 1px solid #CCCCCC;
     margin-bottom: 5px;
-    padding: 5px;}
+    padding: 5px 20px;}
+    #what ul{        margin: 10px 0 20px;
+    padding: 0 0 0 40px;}
+	#what ul li{     list-style: square outside none;
+    padding-bottom: 0.45em;}
+
+    #what h3{    border-bottom: 1px solid #DDDDDD;
+    font-size: 22px;
+    line-height: 30px;
+    margin-bottom: 5px;
+    padding: 5px 0;}
 	</style>
 
 	<!-- CONTENT START -->
 	<div id="red-content">
 		<div class="whybox" id="why"><p>Netcoid dibentuk sebagai ispirasi, teknologi dan media penghubung antar bisnis  yang berada disetiap daerah indonesia. Netcoid adalah jejaring bisnis pertama di Indonesia, dengan menggabungkan semua ide yang terbaik yang pernah ada di web, mulai dari social, profiles, dan sistim pencarian.</p></div>
 
+		<div id="what">
+		<h3>Ikuti Perkembangan bisnis sesuai kebutuhan!</h3>
+		<p>Masih facebookan? bayangkan berapa waktu yang dapat digunakan untuk hal yang lebih bermamfaat. seperti mencari rekan Entrepreneur. Bayangkan jika anda mempunyai toko baju, anda dapat mengikuti usaha seperti toko kain, design indie, hingga ke jasa marketing, Jika anda Investor? ikuti usaha lain yang sekiranya menarik perhatian anda, Kami percaya bahwa dengan informasi, bersama kami dapat meningkatkan profit dan kinerja usaha.
+		</p>
+		<ul>
+			<li>Ikuti perkembangan Rekan Netcoid lainnya, Anda toko baju? ikuti toko kain, jasa design baju hingga ke jasa marketing</li>
+		</ul>
+		</p>		
+		<?php $views->getIMG('what-0.png') ?>
+		<h3>Dapatkan Profile / Portofolio Sekaligus Website!</h3>
+		<ul>
+			<li>Interaksi juga berlaku di B2C, gunakan profile anda sebagai media penghubung antara pembeli!</li>
+			<li>Upload produk, Sebarkan dan tingkatkan penjualan anda</li>
+			<li>Kotak Informasi, Dapat digunakan untuk promosi, cari partner, atau untuk mencari kebutuhan</li>
+			<li>Verifikasi & Sistim Poin, Dapatkan kepecayaan di dunia maya, lengkapnya di <a href="/verify">Verify</a></li>
+		</ul>
+		<?php $views->getIMG('what-1.png'); ?>
+		<h3>Hubungkan Akun anda dengan IM & Jejaring Social!</h3>
+		<ul>
+			<li>Twitter</li>
+			<li>Facebook</li>
+			<li>Yahoo Messenger</li>
+		</ul>
+		<?php $views->getIMG('what-2.png'); ?>
+		<h3>Promosikan Produk Anda!</h3>
+		<ul>
+			<li>Kemudahan, 80% pelanggan anda akan menghubungi anda jika serius ingin membeli sesuatu</li>
+			<li>Informasi produk, gunakan gambar atau penjelasan tambahan untuk memperjelas produk anda</li>
+		</ul>
+		<?php $views->getIMG('what-3.png'); ?>
+
 		<div class="whybox" id="how">Sebagai destinasi dunia maya ( www ), Netcoid adalah sebuah layanan dalam bentuk aplikasi yang membantu saling <strong>menghubungkan, berinteraksi, dan bertransaksi</strong> yang dapat digunakan oleh pengusaha, perusahaan hingga ke organisasi <a href="http://en.wikipedia.org/wiki/Nonprofit_organization">non-profit</a>, dengan cara <strong>semudah, sedekat dan sebebas</strong> mungkin.
 		Saat ini kami membuka kerjasama kepada komunitas non-profit ( seperti komunitas pengrajin, komunitas kalangan wiraswasta mahasiswa, atau komunitas non-profit maupun profit lainnya ), jika berminat, silahkan email ke nonprofit@networks.co.id.
 		</div>
 
-		<div>
-		<h2>Seperti Apa?</h2>
-		<h3>Dapatkan Profile / Portofolio Sekaligus Website!</h3>
-		<?php $views->getIMG('what-1.png'); ?>
-		<h3>Hubungkan Akun anda dengan Jejaring Social!</h3>
-		<?php $views->getIMG('what-2.png'); ?>
-		<h3>Promosikan Produk Anda!</h3>
-		<?php $views->getIMG('what-3.png'); ?>
 		<h3>Lainnya?</h3>
 		anda dapat melihat informasi lainnya di blog netcoid http://www.networks.co.id/blog atau dengan langsung mendaftarkan diri sekarang. untuk informasi ke hello@networks.co.id
 		</div>
