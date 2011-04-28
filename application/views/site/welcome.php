ee259202e4f882399b4e4d398225c9388baa6fa7
Adam Ramadhan <adamramadhan@live.com>
2011-04-28 18:13:33 +0700

    update welcome page

--- application/views/site/welcome.php
+++ application/views/site/welcome.php
@@ -1,4 +1,12 @@
-
+	<style type="text/css" media="screen">
+	#like-facebookapi {
+    display: block;
+    margin: 10px 0 0;
+	}
+	#follow-twitterapi{
+	display: block;
+	}
+	</style>
 		
 	<!-- CONTENT START -->
 	<div class="clearfix" id="red-content">
@@ -8,21 +16,32 @@
 			<a target="_blank" href="terms.php">panduan lengkap</a> website ini.
 			</p></br>
 			<p>Perhatian*, Nomer kontak anda akan dihubungi oleh staff kami paling lama 24x7 
-			oleh kami untuk tindak lanjut verifikasi akun.</p>
+			jika anda mendaftar sebagai perusahaan ( PT dan CV ).</p>
 		</div>
 		<div class="box-approve">
-			<h3>Kenapa Verifikasi</h3>
-			<p>Sebelumnya kami akan mengucapkan selamat kepada anda, anda berada pada 1000 pendaftar pertama. <a href="/verify">verifikasi</a> akan digunakan
-				untuk mendapatkan seal penjual terpercaya*, saat ini kami hanya menerima PT, CV dan Pribadi, nantinya anda akan dihubungi dan dimintai keterangan tentang organisasi anda, setelah itu kami akan meminta scan <a target="_blank" href="help.php">SIUP perusahaan</a> untuk PT dan CV. KTP untuk Private. 
+			<h3>Ikuti Perkembangan kami!</h3>
+			<p>Sebelumnya kami akan mengucapkan selamat kepada anda, anda berada pada 1000 pendaftar pertama. Kami sedang berkembang cepat, untuk mengikuti perkembangan anda dapat membuka <a target="_blank" href="/blog">Blog</a> kami, Twitter dan Facebook kami; 
+			<p class="clearfix" style=" padding: 5px 5px 0;">
+			<span id="follow-twitterapi"></span>
+			<span id="like-facebookapi"><fb:like href="www.facebook.com/netcoid" send="true" width="300" show_faces="true" font=""></fb:like></span>
+			</p>
 			</p>				
 		</div>
 		<div class="box-approve">
 			<h3>Peraturan &amp; Ketentuan</h3>
-			<p>Pada dasarnya ada dua aturan disini:</br> Jangan memessage atau memposting hal yang tidak ada tujuan, bersifat kasar dan terkadang tidak wajar.</p></br>
-			<p>yang kedua adalah prinsip "kesopanan" banyaknya perbedaan komunitas penjual akan membawa banyak orang tidak bertanggung jawab untuk berani mengatakan hal - hal yang menyinggung komunitas penjual lainnya, jadi jangan ucapkan hal yang tidak anda akan ucapkan pada saat melakukan percakapan
+			<p>Pada dasarnya ada dua aturan disini:</br> Jangan memessage atau memposting hal yang tidak terdapat tujuan, bersifat kasar dan terkadang tidak wajar.</p></br>
+			<p>yang kedua adalah prinsip "kesopanan" banyaknya perbedaan komunitas penjual akan membawa banyak rekan tidak bertanggung jawab untuk berani mengatakan hal - hal yang menyinggung komunitas penjual lainnya, jadi jangan ucapkan hal yang tidak anda akan ucapkan pada saat melakukan percakapan
 			tatap muka.</p>				
 		</div>
 		<span class="spanwelcome">
 		setelah anda membaca, anda dapat langsung "masuk" dari menu atas kanan.</span>
 	</div>
 	<!-- CONTENT END -->
+
+	<script src="https://platform.twitter.com/anywhere.js?id=VgMhY8zm9QF6SgpYskmptA&v=1" type="text/javascript"></script> 
+	  <script type="text/javascript">
+		  twttr.anywhere(function (T) {
+		    T('#follow-twitterapi').followButton("netcoid");
+		  });
+	  </script>
+	  <script src="https://connect.facebook.net/en_US/all.js#xfbml=1"></script>
