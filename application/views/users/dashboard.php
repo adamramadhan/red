d8ad6ca9c7612f1763068791c0f56edd64c5040a
Adam Ramadhan <adamramadhan@live.com>
2011-04-30 10:47:50 +0700

    invitasi 1 ke 2

--- application/views/users/dashboard.php
+++ application/views/users/dashboard.php
@@ -1,4 +1,4 @@
-<div style="height: 30px; background: none repeat scroll 0pt 0pt rgb(255, 255, 255); border-bottom: 1px solid rgb(231, 231, 231); color: rgb(68, 68, 68);"><p style="margin: 0pt auto; width: 960px; text-align: center; line-height: 29px;"><span style="position: relative;right: 5px;top: 2px;"><?php $views->getIMG('i/megaphonebybigfunkychiken.gif'); ?> </span>Hallo, Kami sedang voting <a href="/blog?id=6"><u>permintaan fitur #1</u></a> . Jangan lupa untuk <i>Ctrl + D</i> untuk Bookmark!</p></div>
+<div style="height: 30px; background: none repeat scroll 0pt 0pt rgb(255, 255, 255); border-bottom: 1px solid rgb(231, 231, 231); color: rgb(68, 68, 68);"><p style="margin: 0pt auto; width: 960px; text-align: center; line-height: 29px;"><span style="position: relative;right: 5px;top: 2px;"><?php $views->getIMG('i/megaphonebybigfunkychiken.gif'); ?> </span>Hallo, Kami sedang voting <a href="/blog?id=6"><u>permintaan fitur #2</u></a> . Jangan lupa untuk <i>Ctrl + D</i> untuk Bookmark!</p></div>
 
 <!-- UJI COBA -->
 <?php $views->js('jquery','area51/dashboard'); ?>
