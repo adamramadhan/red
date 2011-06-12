eec95fb2d6b96f89ee67ce3685ca865fb39a11b4
Adam Ramadhan <adamramadhan@live.com>
2011-06-12 12:40:52 +0700

    installed local font

--- application/views/site/index-closed-invitation.php
+++ application/views/site/index-closed-invitation.php
@@ -1,7 +1,7 @@
 <div style="height: 30px; background: none repeat scroll 0pt 0pt rgb(255, 255, 255); border-bottom: 1px solid rgb(231, 231, 231); color: rgb(68, 68, 68);"><p style="margin: 0pt auto; width: 960px; text-align: center; line-height: 29px;">
  <?php echo $socialpoint; ?> ♥ Netcoid <span style="background: none repeat scroll 0% 0% rgb(254, 255, 203); padding: 3px 10px;">Invitasi #2 telah ditutup, untuk mendapatkan invitasi berikutnya follow <a style="color: rgb(211, 46, 46);" href="http://www.twitter.com/netcoid" target="_blank">twitter</a>, like <a style="color: rgb(211, 46, 46);" target="_blank" href="http://www.facebook.com/netcoid">facebook</a> atau ikuti <a style="color: rgb(211, 46, 46);" target="_blank" href="/development">perkembangan</a> kami.</span></p></div></div>
 
-	<link  href="https://fonts.googleapis.com/css?family=Pacifico:regular" rel="stylesheet" type="text/css" >
+	
 	<style type="text/css" media="screen">
 	.home{
 		    background: url("www-static/assets/images/trees.png") no-repeat scroll center bottom #FFFFFF !important;
