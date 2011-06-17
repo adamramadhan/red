739bb761ab9c9b3240170b9582ac079c03732b11
Adam Ramadhan <adamramadhan@live.com>
2011-06-17 13:49:23 +0700

    team are testing again and again for the invitation 3

--- application/test.php
+++ application/test.php
@@ -136,7 +136,7 @@ class Test extends Application {
 			$data['insights']['product'] = implode(',', $products);
 		}
 
-		if (count($data['insights']['page']) < 2 || $data['insights']['product']) = 0) {
+		if (count($data['insights']['page']) < 2 || count($data['insights']['product']) = 0) {
 			$data['insights']['null'] = TRUE;
 			#$data['insights']['page'] = '["'.$weekago.'",1,1,1,1],["'.$weektoday.'",1,1,1,1]';
 			#$data['insights']['product'] = '["#1 Product",1,1,1,1],["#2 Product",1,1,1,1]';
