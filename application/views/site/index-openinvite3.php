cf543adb7d86fc407f97d14f82ea55edd583ce3b
Adam Ramadhan <adamramadhan@live.com>
2011-06-23 23:54:13 +0700

    updated a bit

--- application/views/site/index-openinvite3.php
+++ application/views/site/index-openinvite3.php
@@ -4,7 +4,6 @@
 <?php $views->CSS('buttons.v1') ?>	
 	<style type="text/css" media="screen">
 	.whatnetcoid {
-	    display: block;
 	    font-family: 'Pacifico',serif;
 	    font-size: 63px;
 	    font-style: normal;
@@ -16,15 +15,16 @@
 	}
 	.hownetcoid{text-align: center;}
 	.previewnetcoid p {
-	    background: none repeat scroll 0 0 #444444;
+	    background: none repeat scroll 0 0 #222;
 	    color: #FFFFFF;
-	    display: table;
 	    font-weight: bold;
-	    margin: 0 auto;
 	    padding: 0 10px;
-	    padding:5px 10px;
+	    padding:5px 10px;display:block;
+	    width:960px;
+	    text-align:center;
 	}
 	.previewnetcoid{display:none;height:438px !important;}
+	.previewnetcoid img{display:block;}
 	</style>
 
 	<!-- CONTENT START -->
@@ -36,25 +36,35 @@
 			<div style="width: 978px;" id="ajax-loading-preview"><img src="/www-static/assets/images/ajax-loader.gif" style="display: block; margin: 200px auto;"></div>
 			<div class="previewnetcoid">
 				<div><?php $views->getIMG('temp/new0.png'); ?>
-				<p>Hello, Netcoid adalah jejaring bisnis <i>pertama</i> di Indonesia</p></div>
+				<p>Hello, Netcoid adalah jejaring bisnis <i>pertama</i> di Indonesia</p>
+				</div>
 				<div><?php $views->getIMG('temp/new1.png'); ?>
-				<p>Buat <i>profil</i> Perusahaan, Bisnis atau Organisasi anda di www.networks.co.id/bisnisanda</p></div>
+				<p>Buat <i>profil</i> Perusahaan, Bisnis atau Organisasi anda di www.networks.co.id/bisnisanda</p>
+				</div>
 				<div><?php $views->getIMG('temp/new4.png'); ?>
-				<p>Social. Twitter, Facebook dan Yahoo dalam <i>satu halaman</i></p></div>
+				<p>Social. Twitter, Facebook dan Yahoo dalam <i>satu halaman</i></p>
+				</div>
 				<div><?php $views->getIMG('temp/new3.png'); ?>
-				<p>Upload Jasa, Produk atau Portofolio bisnis Anda</p></div>
+				<p>Upload Jasa, Produk atau Portofolio bisnis Anda</p>
+				</div>
 				<div><?php $views->getIMG('temp/new5.png'); ?>
-				<p>Ikuti berita sesuai kebutuhan Anda secara <i>Real-Time</i></p></div>
+				<p>Ikuti berita sesuai kebutuhan Anda secara <i>Real-Time</i></p>
+				</div>
 				<div><?php $views->getIMG('temp/new6.png'); ?>
-				<p>Berikan promosi, feedback hingga <i>penawaran</i>. Semua bisa terjadi</p></div>
+				<p>Berikan promosi, feedback hingga <i>penawaran</i>. Semua bisa terjadi</p>
+				</div>
 				<div><?php $views->getIMG('temp/new7.png'); ?>
-				<p><i>Konsumisasi</i> Produk anda sebebas - bebasnya</p></div>
+				<p><i>Konsumisasi</i> Produk anda sebebas - bebasnya</p>
+				</div>
 				<div><?php $views->getIMG('temp/new2.png'); ?>
-				<p>Insights, <i>Pelajari</i> konsumen anda. Lakukan optimasi</p></div>
+				<p>Insights, <i>Pelajari</i> konsumen anda. Lakukan optimasi</p>
+				</div>
 				<div><?php $views->getIMG('temp/new8.png'); ?>
-				<p>Insights, juga mengembalikan kata <i>fun</i> dalam bisnis</p></div>
+				<p>Insights, juga mengembalikan kata <i>fun</i> dalam bisnis</p>
+				</div>
 				<div><?php $views->getIMG('temp/new9.png'); ?>
-				<p>Blog bersama, dari kompetisi ke <i>kolaborasi</i></p></div>
+				<p>Blog bersama, dari kompetisi ke <i>kolaborasi</i></p>
+				</div>
 			</div>
 
 			<hr>
