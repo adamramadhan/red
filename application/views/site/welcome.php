c9a16b7e4b3456b913ef546e606097823fbd4567
Adam Ramadhan <adamramadhan@live.com>
2011-06-25 20:56:13 +0700

    fix some things

--- application/views/site/welcome.php
+++ application/views/site/welcome.php
@@ -3,12 +3,11 @@
 	    display: block;
 	    margin: 10px 0 0;
 		}
-		#follow-twitterapi {
-		    display: block;
-		    margin-top: 10px;
+		#follow-twitterapi{
+		display: block;
 		}
 	</style>
-		
+	<?php $views->CSS('blog'); ?>
 	<!-- CONTENT START -->
 	<div class="clearfix" id="red-content">
 		<div class="box-approve">
@@ -19,14 +18,13 @@
 			<p>Perhatian*, Nomer kontak anda akan dihubungi oleh staff kami paling lama 24x7 
 			jika anda mendaftar sebagai perusahaan ( PT dan CV ).</p>
 		</div>
-		<div class="box-approve">
+		<div class="clearfix box-approve">
 			<h3>Ikuti Perkembangan kami!</h3>
 			<p>Sebelumnya kami akan mengucapkan selamat kepada anda, anda berada pada 1000 pendaftar pertama. Kami sedang <i>berkembang cepat</i>, untuk mengikuti perkembangan kami anda dapat mengikuti <a target="_blank" href="/blog">Blog</a> kami, Twitter dan Facebook kami; 
-			<p class="clearfix" style=" padding: 5px 5px 0;">
-			<a href="http://twitter.com/masedem" class="twitter-follow-button" data-show-count="false">Follow @masedem</a>
-<script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
+			<div class="clearfix" style=" padding: 10px 0px 0;">
+			<span id="follow-twitterapi"></span>
 			<span id="like-facebookapi"><fb:like href="www.facebook.com/netcoid" send="true" width="300" show_faces="true" font=""></fb:like></span>
-			</p>
+			</div>
 			</p>				
 		</div>
 		<div class="box-approve">
@@ -35,8 +33,15 @@
 			<p>yang kedua adalah prinsip "kesopanan" banyaknya perbedaan komunitas penjual akan membawa banyak rekan tidak bertanggung jawab untuk berani mengatakan hal - hal yang menyinggung komunitas penjual lainnya, jadi jangan ucapkan hal yang tidak anda akan ucapkan pada saat melakukan percakapan
 			tatap muka.</p>				
 		</div>
+		<div class="clearfix"></div>
 		<span class="spanwelcome">
-		setelah anda membaca, Anda dapat "masuk" dari menu atas kanan.</span>
+		setelah anda membaca, anda dapat langsung "masuk" dari menu atas kanan.</span>
 	</div>
 	<!-- CONTENT END -->
+	<script src="https://platform.twitter.com/anywhere.js?id=VgMhY8zm9QF6SgpYskmptA&v=1" type="text/javascript"></script> 
+	<script type="text/javascript">
+	  twttr.anywhere(function(twitter) {  
+	    twitter('#follow-twitterapi').followButton("netcoid");
+	  });
+	</script>
 	<script src="https://connect.facebook.net/en_US/all.js#xfbml=1"></script>
